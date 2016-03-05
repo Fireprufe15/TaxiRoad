0bb4aac3a1c59bce4fc66ca67bc6c90e93ba1b9e
stormmuller <bboystorm.mm@gmail.com>
2016-03-05 23:12:18 +0200

    fixed reset
    reset

--- Assets/Scripts/ReloadLevel.cs
+++ Assets/Scripts/ReloadLevel.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class ReloadLevel : MonoBehaviour {
 
 	public void Reload(){
+		
+		GrassDamage.playerHealth = 100;
+		CamShake.shake = 0;
+		RoadCreator.difficulty = 1;
 		Application.LoadLevel (Application.loadedLevel);
 	}
 
