0cc315960fe62e4c86e9f54fc112afc48cae6521
stormmuller <bboystorm.mm@gmail.com>
2016-03-06 06:10:11 +0200

    someStuff
    asdf

--- Assets/Menu/Scripts/MenuController.cs
+++ Assets/Menu/Scripts/MenuController.cs
@@ -3,9 +3,23 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
+	public GameObject creditsGO;
+	public GameObject settingsGO;
 
 	public void StartGame()
 	{
 		GameObject.Find("Canvas_menu").SetActive (false);
+	}
+
+	public void ShowCredits()
+	{
+		creditsGO.SetActive (true);
+		settingsGO.SetActive (false);
+	}
+
+	public void ShowSettings()
+	{
+		settingsGO.SetActive (true);
+		creditsGO.SetActive (false);
 	}
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Settings : MonoBehaviour {
+
+	static public bool sound;
+	static public float volume;
+
+
+	public void SoundOn()
+	{
+		sound = GameObject.Find ("Toggle").GetComponent<Toggle> ().isOn;
+	}
+
+	public void setVolume()
+	{
+		volume = GameObject.Find ("Slider").GetComponent<Slider> ().value;
+	}
+}
